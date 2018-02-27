dc8d2369566bd8ead28b75e85a0dbaf278f6c12c
1002237 <wonwoo@sk.com>
2018-02-27 16:32:44 +0900

    ios client

---
+++ client/ios/Wasabi/FuntionTestViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Wasabi
+//
+//  Created by 1002237 on 2018. 2. 23..
+//  Copyright © 2018년 1002237. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import AVFoundation
+
+
+
+class FuntionTestViewController: UIViewController {
+
+    @IBOutlet weak var functionTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.functionTableView.delegate = self
+        self.functionTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension FuntionTestViewController : UITableViewDelegate {
+//    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat{
+//        return CGFloat(50.0)
+//    }
+}
+
+extension FuntionTestViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return self.functionTableView.dequeueReusableCell(withIdentifier: "TTSCell")!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
+}
+
+class TTSCell : UITableViewCell{
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var ttsTestLabel: UILabel!
+    
+    @IBAction func touchUpInsidePlayButton(_ sender: UIButton) {
+        let synthesizer = AVSpeechSynthesizer()
+        
+        let utterance = AVSpeechUtterance(string: self.ttsTestLabel.text ?? "")
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        utterance.rate = 0.4
+        
+        synthesizer.speak(utterance)
+    }
+}
+
---
+++ client/ios/WasabiTests/WasabiTests.swift
@@ -0,0 +1,36 @@
+//
+//  WasabiTests.swift
+//  WasabiTests
+//
+//  Created by 1002237 on 2018. 2. 23..
+//  Copyright © 2018년 1002237. All rights reserved.
+//
+
+import XCTest
+@testable import Wasabi
+
+class WasabiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
